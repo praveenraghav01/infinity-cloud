591f6cf9b5eb569f58a25eeeafec11ce842f03ef
Praveen Kumar <praveensinghraghav@users.noreply.github.com>
2017-08-10 21:17:38 +0530

    Config

--- app/config.php
+++ app/config.php
@@ -2,9 +2,9 @@
 
 return [
     's3' => [
-        'key' => 'AKIAI4XUV5LI52MD63ZA',
-        'secret' => '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',
-        'bucket' => 'praveenraghav',
+        'key' => '**************',
+        'secret' => '*************',
+        'bucket' => '**********',
         'region' => 'ap-south-1',
         'version' => '2006-03-01'
     ]
--- app/start.php
+++ app/start.php
@@ -9,10 +9,10 @@ $config = require('config.php');
 $s3 = new Aws\S3\S3Client([
     'version'     => 'latest',
     'region'      => 'ap-south-1',
-    'bucket' => 'praveenraghav',
+    'bucket' => '************av',
     'credentials' => [
-        'key'    => 'AKIAI4XUV5LI52MD63ZA',
-        'secret' => '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'
+        'key'    => '************',
+        'secret' => '*****************2y376N'
     ]
 ]);
 ?>
